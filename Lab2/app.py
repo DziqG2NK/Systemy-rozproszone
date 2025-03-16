a75fb75ddff9324302094f1fa8a64b4ffdf3941a
Marcin Walendzik <mracin.walendzik.dysk@gmail.com>
2025-03-16 23:20:59 +0100

    Basic sollution for 2nd labolatory's exercise

--- Lab2/app.py
+++ Lab2/app.py
@@ -2,31 +2,57 @@ from fastapi import FastAPI
 
 app=FastAPI( )
 
-@app.get("/")
-async def root() :
-    return {"message" : "Welcome to Pole System!"}
+class Vote:
+    def __init__(self, id: int, name: str):
+        self.id = id
 
 class Poll:
-    def __init__(self, pollId: int, question: str):
-        self.pollId = pollId
-        self.question = question
+    def __init__(self, id: int):
+        self.id = id
         self.votes = {}
 
-    def add_option(self, option: str):
-        if option not in self.votes:
-            self.votes[option] = 0
-
-    def add_vote(self, option: str):
-        if option in self.votes:
-            self.votes[option] += 1
-            return True
+    def add_vote(self, vote: Vote):
+        if vote.id not in self.votes.keys():
+            self.votes[vote.id] = 1
         else:
-            return False
+            self.votes[vote.id] += 1
 
-    def delete_vote(self, option: str):
-        if option in self.votes:
-            if self.votes[option] > 0:
-                self.votes[option] -= 1
-                return True
-        else:
-            return False
+biggest_id = 1
+polls_array = []
+
+@app.get("/")
+async def root() :
+    return {"message" : "Take your poll!"}
+
+@app.get("/poll")
+async def get_all_polls():
+    return polls_array
+
+@app.put("/poll")
+async def create_new_poll():
+    global biggest_id
+    polls_array.append(Poll(biggest_id))
+    biggest_id += 1
+    return {"message" : f"New poll id is {biggest_id - 1}"}
+
+@app.delete("/poll/{poll_id}/votes/{vote_id}")
+async def remove_one_vote(poll_id: int, vote_id: int):
+    for i, i_poll in enumerate(polls_array):
+        if i_poll.id == poll_id and vote_id in i_poll.votes.keys() and i_poll.votes[vote_id] > 0:
+            polls_array[i].votes[vote_id] -= 1
+            return {"message" : f"Removed vote for option {vote_id} in poll {poll_id}"}
+
+    return {"message": f"Removing not completed"}
+
+@app.post("/poll/{poll_id}/votes/{vote_id}")
+async def add_vote_to_poll(poll_id: int, vote_id: int):
+    for i, i_poll in enumerate(polls_array):
+        if i_poll.id == poll_id:
+            if vote_id not in i_poll.votes.keys():
+                polls_array[i].votes[vote_id] = 1
+                return {"message": f"Created option {vote_id} in poll {poll_id}"}
+
+            polls_array[i].votes[vote_id] += 1
+            return {"message" : f"Added vote for option {vote_id} in poll {poll_id}"}
+
+    return {"message": f"Voting not succeeded"}
