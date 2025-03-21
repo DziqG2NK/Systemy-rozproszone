15f59e25f296f29f00284401d966537c7d6e8b35
Marcin Walendzik <mracin.walendzik.dysk@gmail.com>
2025-03-21 15:59:37 +0100

    Simple API finished

--- Exercises/RestAPI/books.py
+++ Exercises/RestAPI/books.py
@@ -1,7 +1,7 @@
 # Based on course under link:
 # https://www.youtube.com/watch?v=MCVcAAoDJS8
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from uuid import UUID
 
@@ -25,3 +25,28 @@ def create_book(book: Book):
     all_titles.append(book)
     return book     # zwróci jeśli się uda, po to, żeby user wiedział co dodał
 
+@app.put("/{book_id}")
+def update_book(book_id: UUID, book: Book):
+    cnt = 0
+
+    for book_p in all_titles:
+        cnt += 1
+        if book_p.id == book_id:
+            all_titles[cnt - 1] = book
+            return all_titles[cnt - 1]
+    raise HTTPException(
+        status_code=404,
+        detail=f"ID {book_id} : does not exist"
+    )
+
+@app.delete("/{book_id}")
+def delete_book(book_id: UUID):
+
+    for i, book in enumerate(all_titles):
+        if book.id == book_id:
+            del all_titles[i]
+            return f"ID {book_id} : deleted"
+    raise HTTPException(
+        status_code=404,
+        detail=f"ID {book_id} : does not exist"
+    )
