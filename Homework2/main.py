c98e95c85372ff8a40f2611ad4f766b01500450f
Marcin Walendzik <mracin.walendzik.dysk@gmail.com>
2025-03-22 03:00:55 +0100

    'Hello API!' in homework fastAPI

---
+++ Homework2/main.py
@@ -0,0 +1,20 @@
+# Small API app for listing people on ISS and current location of ISS
+# http://open-notify.org/Open-Notify-API/?ref=public_apis&utm_medium=website
+# https://opencagedata.com/api#quickstart
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+geo_api_key = "e2357fc9ddfb401bade0d477f1f0ce7e"
+
+geo_api_url = "https://api.opencagedata.com/geocode/v1/"
+iss_location_api_url = "http://api.open-notify.org/iss-now.json"
+astronauts_list_url = "http://api.open-notify.org/astros.json"
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def get_position(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
