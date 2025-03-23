3399d942b16c776440acd8ee41d43f3895c0e395
Marcin Walendzik <mracin.walendzik.dysk@gmail.com>
2025-03-23 22:27:25 +0100

    Half of links data received

---
+++ Homework2/data.py
@@ -0,0 +1,55 @@
+import httpx
+import asyncio
+import json
+
+async def get_info_from_api(url: str, params=None):
+    async with httpx.AsyncClient() as httpx_client:
+        response = await httpx_client.get(url=url, params=params)
+        print(response.request)
+
+        if response.status_code != 200:
+            return {"error": "Response from iss error"}
+
+        return response.text
+
+async def turn_response_to_json(response):
+
+
+async def main():
+    geo_api_key = "e2357fc9ddfb401bade0d477f1f0ce7e"
+    geo_q = "52.5432379+13.41421330"
+    geo_q = "52.5432379%2C+13.4142133"
+
+    geo_api_url = "https://api.opencagedata.com/geocode/v1/json"
+    satelite_location_url = "https://tle.ivanstanojevic.me/api/tle/43696"
+    iss_location_api_url = "http://api.open-notify.org/iss-now.json"
+    astronauts_list_url = "http://api.open-notify.org/astros.json"
+
+    geo_params = {
+        "key": geo_api_key,
+        "q": geo_q,
+        "pretty": 1,
+        "no_annotations": 1
+    }
+
+    URLS = []
+
+    URLS.append((geo_api_url, geo_params))
+    URLS.append(satelite_location_url)
+    URLS.append(iss_location_api_url)
+    URLS.append(astronauts_list_url)
+
+    for url in URLS:
+        r = None
+        if not isinstance(url, tuple):
+            r = await get_info_from_api(url)
+        elif isinstance(url, tuple):
+            r = await get_info_from_api(url[0], url[1])
+            print(type(r))
+            r = json.loads(r)
+            print(type(r))
+        print(r)
+        print(r["results"])
+
+asyncio.run(main())
+# print(type((1,2)))
--- Homework2/main.py
+++ Homework2/main.py
@@ -8,22 +8,10 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 import httpx
 
-async def get_info_from_api(url, params):
-    async with httpx.AsyncClient() as httpx_client:
-        if params is None:
-            response = await httpx_client.get(url=url, params=params)
-        elif params is not None:
-            response = await httpx_client.get(url=url, params=params)
-
-        if response.status_code != 200:
-            return {"error": "Response from iss error"}
-
-        return response.text
-
 geo_api_key = "e2357fc9ddfb401bade0d477f1f0ce7e"
 
 geo_api_url = "https://api.opencagedata.com/geocode/v1/"
-satelite_location_url = "https://tle.ivanstanojevic.me/#/tle/"
+satelite_location_url = "https://tle.ivanstanojevic.me/#/tle/43786"
 iss_location_api_url = "http://api.open-notify.org/iss-now.json"
 astronauts_list_url = "http://api.open-notify.org/astros.json"
 
@@ -36,8 +24,8 @@ async def get_position(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/getJSON")
-async def get_json():
-    r = await get_info_from_api("http://api.open-notify.org/astros.json", params=None)
+async def get_json(url: str):
+    r = await get_info_from_api(url, params=None)
     print(r)
     return
     # return get_info_from_api("http://api.open-notify.org/astros.json", params=None)
