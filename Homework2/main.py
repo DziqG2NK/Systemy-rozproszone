f9266a10385af9c04ac9556e4475dbbec61fff4e
Marcin Walendzik <mracin.walendzik.dysk@gmail.com>
2025-03-24 01:43:31 +0100

    Passing satellite name from form

--- Homework2/main.py
+++ Homework2/main.py
@@ -6,14 +6,6 @@
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-import httpx
-
-geo_api_key = "e2357fc9ddfb401bade0d477f1f0ce7e"
-
-geo_api_url = "https://api.opencagedata.com/geocode/v1/"
-satelite_location_url = "https://tle.ivanstanojevic.me/#/tle/43786"
-iss_location_api_url = "http://api.open-notify.org/iss-now.json"
-astronauts_list_url = "http://api.open-notify.org/astros.json"
 
 app = FastAPI()
 
@@ -23,17 +15,51 @@ templates = Jinja2Templates(directory="templates")
 async def get_position(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/getJSON")
-async def get_json(url: str):
-    r = await get_info_from_api(url, params=None)
-    print(r)
-    return
-    # return get_info_from_api("http://api.open-notify.org/astros.json", params=None)
+@app.get("/response")
+async def get_response(request: Request, satellite_name: str=""):
 
-@app.get("/getsomething")
-async def get_some():
-    r = httpx.get("http://127.0.0.1:8000/")
-    return r.text
+
+
+    satellite = {
+        "name": satellite_name,
+        "country": "gfd",
+        "latitude": "saaa",
+        "longitude": "aaaa"
+    }
+
+    return templates.TemplateResponse("response.html", {
+            "request": request,
+            "satellite": satellite
+        })
+
+# @app.get("/response")
+# async def get_response(request: Request):
+#     return templates.TemplateResponse("response.html", {
+#         "request": request,
+#         "satellite": {
+#             "name": "bhgfdkjk",
+#             "country": "gfd",
+#             "latitude": "saaa",
+#             "longitude": "aaaa"
+#
+#         }
+#
+#     })
+
+
+
+# @app.get("/getJSON")
+# async def get_json(url: str):
+#     r = await get_info_from_api(url, params=None)
+#     print(r)
+#     return
+#     # return get_info_from_api("http://api.open-notify.org/astros.json", params=None)
+#
+# @app.get("/getsomething")
+# async def get_some():
+#     r = httpx.get("http://127.0.0.1:8000/")
+#     return r.text
 # @app.get("/gfd/")
 # async def get_position(request: Request):
     # return templates.TemplateResponse("response.html", {"request": request, "satelite": })
+
