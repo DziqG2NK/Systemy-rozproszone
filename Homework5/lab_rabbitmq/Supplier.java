1160fa39f2446f083d645ccafa85837d5ff530ae
DziqG2NK <mracin.walendzik.dysk@gmail.com>
2025-06-02 03:33:10 +0200

    Some changes

--- Homework5/lab_rabbitmq/Supplier.java
+++ Homework5/lab_rabbitmq/Supplier.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Supplier {
+    private static String supplierName;
+    private static ArrayList<String> supplierProducts;
+
+    public Supplier(String supplierName) {
+        this.supplierName = supplierName;
+        this.supplierProducts = new ArrayList<>();
+    }
+
+    public void addProduct(String productName) {
+        supplierProducts.add(productName);
+    }
+
+    public void getInfoAboutProducts() {
+        System.out.println("\nSupplier name: " + supplierName + "\n" + "Supplier products: \n");
+        for (String supplierProduct : supplierProducts) {
+            System.out.println("- " + supplierProduct);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter supplier name: ");
+            String input = sc.nextLine();
+            System.out.println();
+
+            Supplier supplier = new Supplier(input);
+
+            System.out.print("Enter products names: ");
+            while (true) {
+                String product = sc.nextLine();
+
+                if (product.isEmpty()) {
+                    continue;
+                }
+
+                else if (product.equals("NEXT")) {
+                    break;
+                }
+
+                else {
+                    supplier.addProduct(product);
+                }
+            }
+
+            supplier.getInfoAboutProducts();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
