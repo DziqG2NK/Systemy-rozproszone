0bb6681320838fff55203d1e348bdd8e5bda0f72
DziqG2NK <mracin.walendzik.dysk@gmail.com>
2025-06-02 04:59:15 +0200

    Working communication without signature

--- Homework5/lab_rabbitmq/Supplier.java
+++ Homework5/lab_rabbitmq/Supplier.java
@@ -1,9 +1,19 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
 
 public class Supplier {
     private String supplierName;
     private ArrayList<String> supplierProducts;
+
+    private Channel orderChannel;
+    private Connection connection;
 
     public Supplier(String supplierName) {
         this.supplierName = supplierName;
@@ -18,6 +28,59 @@ public class Supplier {
         System.out.println("\nSupplier name: " + this.supplierName + "\n" + "Supplier products: \n");
         for (String supplierProduct : this.supplierProducts) {
             System.out.println("- " + supplierProduct);
+        }
+    }
+
+    public void listen() throws IOException {
+
+        System.out.println("Waiting for orders...");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String product = new String(delivery.getBody(), "UTF-8");
+
+            if (supplierProducts.contains(product)) {
+                System.out.println("Received and accepted order for: " + product);
+                for (int i = 1; i <= 5; i++) {
+                    System.out.println(i + "...");
+                }
+            }
+            else {
+                System.out.println("Received but IGNORED order for: " + product);
+            }
+
+            orderChannel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+        };
+
+        orderChannel.basicConsume("orders", false, deliverCallback, consumerTag -> {});
+    }
+
+    public void connect() {
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+
+            this.connection = factory.newConnection();
+            this.orderChannel = connection.createChannel();
+
+            this.orderChannel.queueDeclare("orders", false, false, false, null);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            disconnect();
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if (this.orderChannel != null) {
+                orderChannel.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+            System.out.println("Disconnected from RabbitMQ");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -49,6 +112,8 @@ public class Supplier {
             }
 
             supplier.getInfoAboutProducts();
+            supplier.connect();
+            supplier.listen();
         }
         catch (Exception e) {
             e.printStackTrace();
--- Homework5/lab_rabbitmq/Team.java
+++ Homework5/lab_rabbitmq/Team.java
@@ -1,10 +1,87 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 import java.util.Scanner;
 
 public class Team {
     private String teamName;
+    private Channel ordersChannel;
+//    private Channel adminChannel;
+    private Connection connection;
 
     public Team(String teamName) {
         this.teamName = teamName;
+    }
+
+    public void connect() {
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            this.connection = connection;
+
+            this.ordersChannel = connection.createChannel();
+//            this.adminChannel = connection.createChannel();
+
+            ordersChannel.queueDeclare("orders", false, false, false, null);
+//            adminChannel.queueDeclare("admin-info", false, false, false, null);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            disconnect();
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if (ordersChannel != null) {
+                this.ordersChannel.close();
+            }
+//            if (ordersChannel != null) {
+//                this.adminChannel.close();
+//            }
+            if (ordersChannel != null) {
+                this.connection.close();
+            }
+
+            System.out.println("Succesfully disconnected");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void orderProduct() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Order product: ");
+
+            while (true) {
+                String product = sc.nextLine();
+
+                if (product.isEmpty()) {
+                    continue;
+                }
+
+                else if (product == "QUIT") {
+                    break;
+                }
+
+                else {
+                    try {
+                        this.ordersChannel.basicPublish("", "orders", null, product.getBytes());
+                        System.out.println("Sent: " + product);
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        finally {
+            disconnect();
+        }
     }
 
     public static void main(String[] args) {
@@ -17,28 +94,12 @@ public class Team {
 
             Team team = new Team(input);
 
-            System.out.print("Order product: ");
-            while (true) {
-                String product = sc.nextLine();
-
-                if (product.isEmpty()) {
-                    continue;
-                }
-
-                else if (product.equals("NEXT")) {
-                    break;
-                }
-
-                else {
-                    continue;
-//                    TODO
-                }
-            }
+            team.connect();
+            team.orderProduct();
         }
         catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
+            sc.close();
         }
     }
 }
